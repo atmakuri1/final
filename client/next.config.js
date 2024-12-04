ee0495854bdca9a115983a2806bf7868ae697a51
atmakuri1 <142838275+atmakuri1@users.noreply.github.com>
2024-12-04 13:07:49 -0600

    final change maybe

--- client/next.config.js
+++ client/next.config.js
@@ -1,11 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
 };
+
+module.exports = nextConfig;
